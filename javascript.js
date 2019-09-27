bb578a15793fe59c4af9a6c7a062b9208ede2e55
ngoduc <ngoducnk@gmail.com>
2019-09-27 10:18:55 +0700

    luyen tap if-else

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function getcheck() {
+    var reply;
+    reply=document.getElementById('check').value;
+    if (reply=="ECMAScript"){
+        alert("right");
+    }else{
+        alert("Didn’t know? ECMAScript!");
+    }
+}
